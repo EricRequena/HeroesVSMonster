07d01b0b3b3ecf3b3b5a520dbb8cb645953f311f
EricRequena <eric.requena.7e7@itb.cat>
2024-01-23 18:37:39 +0100

    NameCharacters closes #15

--- ClassLibraryHeroesVsMonster/Class1.cs
+++ ClassLibraryHeroesVsMonster/Class1.cs
@@ -16,5 +16,29 @@
             }
             Console.Clear(); // O cualquier otra acción que desees después de presionar espacio
         }
+
+        public static string[] GetHeroNamesInput() /*Introducir nombres de heroes*/
+        {
+            const int expectedHeroCount = 4; /*Numero de heroes*/
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Introduce los nombres de los 4 héroes separados por un espacio:"); /*Mensaje de introducir nombres*/
+            Console.ForegroundColor = ConsoleColor.White;
+            string input = Console.ReadLine(); /*Introducir nombres*/
+            string[] heroNames = input.Split(' '); /*Separar nombres*/
+
+            while (heroNames.Length != expectedHeroCount) /*Si no se introducen 4 nombres*/
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Debes introducir exactamente {expectedHeroCount} nombres. Vuelve a intentarlo:");
+                Console.ForegroundColor = ConsoleColor.White;
+                input = Console.ReadLine();
+                heroNames = input.Split(' ');
+            }
+
+            return heroNames; /*Devolver nombres*/
+        }
     }
 }
